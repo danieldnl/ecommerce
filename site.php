05a485b77474904313793a60dc64aa48d128fc10
Daniel Carneiro Miranda <daniel.c.miranda@terra.com.br>
2018-07-22 21:23:27 -0300

    site produtos

--- site.php
+++ site.php
@@ -1,10 +1,14 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function () {
-	$page = new Page();
-	$page->setTpl("index");
+    $products = Product::listAll();
+    $page = new Page();
+	$page->setTpl("index", [
+        "products" => Product::checkList($products)
+    ]);
 });
 
 $app->get('/teste/:nome', function ($nome) {
